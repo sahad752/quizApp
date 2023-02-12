e137b8496a96b866b5f6458985ef461422473c3b
sahad752 <sahusahad752@gmail.com>
2023-02-12 10:04:45 +0530

    add alembic for database migrations

--- alembic/env.py
+++ alembic/env.py
@@ -2,6 +2,8 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
+from models.models import Base
+from routers.routes import engine
 
 from alembic import context
 
@@ -18,7 +20,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -38,7 +40,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url  = 'postgresql://postgres:password@localhost/fastapi_database'
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -57,11 +59,7 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    connectable = engine
 
     with connectable.connect() as connection:
         context.configure(
